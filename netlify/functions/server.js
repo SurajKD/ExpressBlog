487af6abea4db11388e334e286b3da01995ccadc
unknown <kdsuraj786@gmail.com>
2025-03-20 00:14:02 +0530

    netlify

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -8,6 +8,5 @@ app.engine('handlebars', require('express-handlebars').engine());
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, '../../static')));
-app.use('/', require(path.join(__dirname, '../../routes/blog.js')));
-
+app.use('/', require(path.resolve(__dirname, '../../routes/blog.js')));
 module.exports.handler = serverless(app);
